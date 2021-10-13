a2aea6fb80a2b7b448de58159167ea00b84e1ff5
John Co <john.co@liferay.com>
2021-10-12 17:51:28 -0700

    Percy initial setup

--- test/liferay-jco.test.js
+++ test/liferay-jco.test.js
@@ -1,61 +1,84 @@
-'use strict';
-
-const { Builder, By } = require('selenium-webdriver');
+//const chrome = require('selenium-webdriver/chrome');
+const { Builder } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
-const { Eyes, Target } = require('@applitools/eyes-selenium');
-const {
-  RectangleSize
-} = require('@applitools/eyes-sdk-core');
+const percySnapshot = require('@percy/selenium-webdriver');
 
+const options = new chrome.Options();
 
-describe('Demo App - Basic', function () {
-  let eyes, driver;
+// if (process.env.CI === 'true') options.headless();
 
-  beforeEach(async () => {
-    // Create Eyes object
+(async function example() {
+  // let driver = await new Builder().forBrowser('chrome').build();
 
-    eyes = new Eyes();
+  let driver = await new Builder()
+  .forBrowser('chrome')
+  .setChromeOptions(options)
+  .build();
 
-    // Optional: Set batch name for tests
+  try {
+    await driver.get('http://google.com/');
+    await percySnapshot(driver, 'Google Homepage');
 
-    eyes.setBatch({ name: 'Demo Test - liferay.com' });
-
-    // Use Chrome browser
-    const options = new chrome.Options();
-
-    if (process.env.CI === 'true') options.headless();
-
-    driver = await new Builder()
-      .forBrowser('chrome')
-      .setChromeOptions(options)
-      .build();
-  });
-
-  it('Smoke Test', async () => {
-    // Navigate to the url we want to test
-    // ⭐️ Note to see visual bugs, 1st run will automatically be set as the baseline.
-    // subsequent runs will compare against baseline run until diff is accepted on applitools.com
+    await driver.get('http://example.com/');
+    await percySnapshot(driver, 'Example Site');
 
     await driver.get('http://localhost:8080');
+    await percySnapshot(driver, 'Liferay Site');
 
-    // Label App name, testcase name and browser screensize
-    await eyes.open(driver, 'Liferay Portal App Name', 'new testcase name', new RectangleSize(800, 600))
-
-    // Take a screenshot of the full page, label the screenshot "Landing Page"
-    await eyes.check('Clay Alert Component', Target.window().fully());
-
-    // Terminates the test
-    await eyes.close();
-
-  });
-
-  afterEach(async () => {
-    // Close the browser
-
+  } finally {
     await driver.quit();
+  }
+})();
 
-    // If the test was aborted before eyes.close was called, ends the test as aborted.
 
-    await eyes.abort();
-  });
-});
+// describe('Demo App - Basic', function () {
+//   let eyes, driver;
+
+//   beforeEach(async () => {
+//     // Create Eyes object
+
+//     eyes = new Eyes();
+
+//     // Optional: Set batch name for tests
+
+//     eyes.setBatch({ name: 'Demo Test - liferay.com' });
+
+//     // Use Chrome browser
+//     const options = new chrome.Options();
+
+//     if (process.env.CI === 'true') options.headless();
+
+//     driver = await new Builder()
+//       .forBrowser('chrome')
+//       .setChromeOptions(options)
+//       .build();
+//   });
+
+//   it('Smoke Test', async () => {
+//     // Navigate to the url we want to test
+//     // ⭐️ Note to see visual bugs, 1st run will automatically be set as the baseline.
+//     // subsequent runs will compare against baseline run until diff is accepted on applitools.com
+
+//     await driver.get('http://localhost:8080');
+
+//     // Label App name, testcase name and browser screensize
+//     await eyes.open(driver, 'Liferay Portal App Name', 'new testcase name', new RectangleSize(800, 600))
+
+//     // Take a screenshot of the full page, label the screenshot "Landing Page"
+//     await eyes.check('Clay Alert Component', Target.window().fully());
+
+//     // Terminates the test
+//     await eyes.close();
+
+//   });
+
+//   afterEach(async () => {
+//     // Close the browser
+
+//     await driver.quit();
+
+//     // If the test was aborted before eyes.close was called, ends the test as aborted.
+
+//     await eyes.abort();
+//   });
+// });
